c691f1b8ff72e361e89497161ec82cb30f766845
Abdulaziz Karimli <abdulaziz.karimli@code.edu.az>
2022-02-20 00:05:08 +0400

    setting serivce methods added

---
+++ src/HMS.Service/Services/Implementations/SettingService.cs
@@ -0,0 +1,39 @@
+﻿using HMS.Core;
+using HMS.Core.Entities;
+using HMS.Service.DTOs.SettingDtos;
+using HMS.Service.Exceptions;
+using HMS.Service.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Service.Services.Implementations
+{
+    public class SettingService : ISettingService
+    {
+        public readonly IUnitOfWork _unitOfWork;
+
+        public SettingService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task EditAsync(int id, SettingItemPostDto settingDto)
+        {
+            Setting setting = await _unitOfWork.SettingRepository.GetAsync(x => x.Id == id);
+            if (setting is null)
+                throw new ItemNotFoundException($"Item not found by id: {id}");
+
+            setting.Value = settingDto.Value;
+
+            await _unitOfWork.CommitAsync();
+        }
+
+        public async Task<Dictionary<string, string>> GetSettingsAsync()
+        {
+            return await _unitOfWork.SettingRepository.GetKeyValueSettingsAsync();
+        }
+    }
+}
