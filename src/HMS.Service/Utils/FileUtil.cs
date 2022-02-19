4002c5340d3b880881658e15e806663c17030de5
Abdulaziz Karimli <abdulaziz.karimli@code.edu.az>
2022-02-20 02:14:30 +0400

    fireBase file upload util added

---
+++ src/HMS.Service/Utils/FileUtil.cs
@@ -0,0 +1,47 @@
+﻿using Firebase.Auth;
+using Firebase.Storage;
+using HMS.Service.Enums;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HMS.Service.Utils
+{
+    public static class FileUtil
+    {
+        public static async Task FireBaseFileUploadAsync(FileStream stream, IFormFile formFile, FileType fileType)
+        {
+            var fileName = $"{Guid.NewGuid()}-{formFile.FileName}";
+
+            var auth = new FirebaseAuthProvider(new FirebaseConfig(Constants.FireBaseApiKey));
+            var a = await auth.SignInWithEmailAndPasswordAsync(Constants.FireBaseAuthEmail, Constants.FireBaseAuthPassword);
+
+            var cancellation = new CancellationTokenSource();
+
+            var task = new FirebaseStorage(
+                Constants.FireBaseBucket,
+                new FirebaseStorageOptions
+                {
+                    AuthTokenAsyncFactory = () => Task.FromResult(a.FirebaseToken),
+                    ThrowOnCancel = true
+                })
+                .Child(fileType == FileType.Image ? "Images" : "Videos")
+                .Child(fileName)
+                .PutAsync(stream, cancellation.Token);
+
+            try
+            {
+                string link = await task;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception was thrown: {0}", ex);
+            }
+        }
+    }
+}
