b4f66a09b16ee5592fba23d2f58cde3a8ba8bf20
Abdulaziz930 <abdulazizak@code.edu.az>
2022-03-07 01:02:25 +0400

    feat: add manage setting controller and update appsettin

---
+++ src/HMS.Admin/Controllers/SettingController.cs
@@ -0,0 +1,33 @@
+﻿using HMS.Admin.Resources.SettingResources;
+using HMS.Admin.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HMS.Admin.Controllers
+{
+    public class SettingController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public SettingController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            HttpResponseMessage httpResponse = await _httpClient.GetAsync($"{Constants.baseUrl}/api/Settings");
+            var responseJsonStr = await httpResponse.Content.ReadAsStringAsync();
+
+            if(httpResponse.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                SettingItemResource settingItem = JsonConvert.DeserializeObject<SettingItemResource>(responseJsonStr);
+                return View(settingItem);
+            }
+
+            return BadRequest();
+        }
+    }
+}
