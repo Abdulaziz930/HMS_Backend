93a1aa22104c37ba475173e7af30815e7dc3331d
Abdulaziz930 <abdulazizak@code.edu.az>
2022-03-07 00:50:46 +0400

    feat: add setting resouces

---
+++ src/HMS.Admin/Resources/SettingResources/SettingItemPostResource.cs
@@ -0,0 +1,16 @@
+﻿using HMS.Service.Enums;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace HMS.Admin.Resources.SettingResources
+{
+    public class SettingItemPostResource
+    {
+        [Required, MaxLength(500)]
+        public string Value { get; set; }
+
+        public IFormFile File { get; set; }
+
+        public FileType FileType { get; set; }
+    }
+}
---
+++ src/HMS.Admin/Resources/SettingResources/SettingItemResource.cs
@@ -0,0 +1,9 @@
+﻿namespace HMS.Admin.Resources.SettingResources
+{
+    public class SettingItemResource
+    {
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+    }
+}
